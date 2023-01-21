760f3c359962d01090ab10bb64fef6a8977ac1f3
vietthom <thomle0418@gmail.com>
2023-01-21 11:20:49 -0700

    Edit photos, social links, and header

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -251,7 +251,7 @@ export default function Home({ articles }) {
     <>
       <Head>
         <title>
-          Spencer Sharp - Software designer, founder, and amateur astronaut
+          Thom Le - Software Engineer and Project Manager
         </title>
         <meta
           name="description"
@@ -261,32 +261,29 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Software Engineer and Project Manager.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I’m Thom, a software engineer and project manager based in Denver, CO. I am an aspiring web developer interested in creating unique UI/UX for the masses. When I'm not coding, I work as a Project Manager for large Environmental annd Civil Engineering firm.  
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
-              href="https://twitter.com"
+              href="https://twitter.com/vietthom"
               aria-label="Follow on Twitter"
               icon={TwitterIcon}
             />
             <SocialLink
-              href="https://instagram.com"
+              href="https://www.instagram.com/big_tunnaa/"
               aria-label="Follow on Instagram"
               icon={InstagramIcon}
             />
             <SocialLink
-              href="https://github.com"
+              href="https://github.com/vietthom"
               aria-label="Follow on GitHub"
               icon={GitHubIcon}
             />
             <SocialLink
-              href="https://linkedin.com"
+              href="https://www.linkedin.com/in/thom-le-b87056138/"
               aria-label="Follow on LinkedIn"
               icon={LinkedInIcon}
             />
